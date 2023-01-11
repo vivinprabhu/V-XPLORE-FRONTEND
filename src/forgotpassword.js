6bf9a361ff5a5773cdf2e549e4767da732c05287
unknown <vivinprabhu001@gmail.com>
2023-01-11 22:45:45 +0530

    vivin

--- src/forgotpassword.js
+++ src/forgotpassword.js
@@ -10,10 +10,82 @@ import {Link} from "react-router-dom";
 function Forgotpassword(){
     const [passwordShown, setPasswordShown] = useState(false);
     const togglePassword = () => {
-    setPasswordShown(!passwordShown);
-    };
+    setPasswordShown(!passwordShown);}
 
-   
+    const [field1, setField1] = useState('');
+    const [field2, setField2] = useState('');
+    const [name, setName] = useState('');
+   const [passwordError, setPasswordError] = useState('');
+   const [passwordError1, setPasswordError1] = useState('');
+   const [nameError, setNameError] = useState('');
+
+    const [Error, setError] = useState('');
+
+    function handleSubmit(event) {
+        if (field1 !== field2) {
+            setError('Passwords are not equal');
+            event.preventDefault()
+        } else {
+          setError('');
+        }
+    
+    if(!name){
+        setNameError('This Field is Required');
+        if(!field1){
+            setPasswordError('This Field is Required');
+            if(!field2){
+                setPasswordError1('This Field is Required');
+                event.preventDefault();
+                return ;
+            }else{
+                setPasswordError1('');
+                
+            }
+            event.preventDefault();
+            return;
+        }else{
+            setPasswordError('');
+            if(!field2){
+                setPasswordError1('This Field is Required');
+                event.preventDefault();
+                return ;
+            }else{
+                setPasswordError1('');
+                
+            }
+        }
+        event.preventDefault();
+        return;
+    }
+    else{
+        setNameError('');
+        if(!field1){
+            setPasswordError('This Field is Required');
+            if(!field2){
+                setPasswordError1('This Field is Required');
+                event.preventDefault();
+                return ;
+            }else{
+                setPasswordError1('');
+                
+            }
+            event.preventDefault();
+            return;
+        }else{
+            setPasswordError('');
+            if(!field2){
+                setPasswordError1('This Field is Required');
+                event.preventDefault();
+                return ;
+            }else{
+                setPasswordError1('');
+                
+            }
+        }
+      
+    }
+};
+
     return(
         <>
             <div class="background3">
@@ -29,23 +101,31 @@ function Forgotpassword(){
                 <div class='s3'><p> V</p></div>
                 <div class='s4'><p>-XPLORE</p></div>
                 <div class='s1'>Forgot Password</div>
-                <div class='s2' ><p> Enter E-mail Address:</p> <br></br><p>Create Password:</p><br></br><p>Confirm Password:</p></div>
 
                 <div class="input4">
                     <div class="input-container">
-                        <input class="input-field7" type="email" placeholder="E-mail" required></input>
+                        <input class="input-field7" type="email" placeholder="E-mail" value={name} onChange={(event) => setName(event.target.value)}/>
                     </div> 
+                        <div className='alerttexts1'>
+                            {nameError && <div style={{color: 'red'}}>{nameError}</div>}
+                        </div>
 
-                    <br></br>
+                        <br></br>
 
                     <div class="input-container">
-            |             <input class="input-field7" type={passwordShown ? "text" : "password" } placeholder="New Password" minLength={8} required></input>
+            |            <input class="input-field7" type={passwordShown ? "text" : "password" } placeholder="New Password" value={field1} onChange={handleSubmit} onChange={e=>{setField1(e.target.value);}} />
                     </div>
+                        <div className='alertpasswords1'>{passwordError && <div style={{color: 'red'}}>{passwordError}</div>}</div>
 
                     <br></br>
 
                     <div class="input-container">
-            |             <input class="input-field7" type={passwordShown ? "text" : "password" } placeholder="Confirm Password" minLength={8} required></input>
+            |             <input class="input-field7" type={passwordShown ? "text" : "password" } placeholder="Confirm Password" value={field2} onChange={handleSubmit} onChange={e=>{setField2(e.target.value);}} />
+                    </div>
+                        <div className='alertpasswords2'>{passwordError1 && <div style={{color: 'red'}}>{passwordError1}</div>}</div>
+
+                    <div class="fperr">
+                        {Error && <div style={{color: 'red'}}>{Error}</div>}
                     </div>
 
                     <div class="showpassword2">
@@ -54,7 +134,13 @@ function Forgotpassword(){
                     
                 </div> 
 
-                <Link to="/login"><input class='fpbtn' type="submit" value="RESET" /></Link>
+                <Link to="/login">
+                    <input class='fpbtn' type="submit" value="RESET" onClick={handleSubmit}/>
+                </Link>
+                
+
+
+
 
             </form>
         </>
